16bdbdc548abe291d6940b8df75a43f962211583
manojkumardevops89 <manojkumar3177arusam@gmail.com>
2026-03-25 11:29:05 +0530

    Update providers.tf

--- Terraform/providers.tf
+++ Terraform/providers.tf
@@ -1,12 +1,21 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
+provider "aws" {
+  region = var.region
+}
+
+provider "kubernetes" {
+  host                   = aws_eks_cluster.main.endpoint
+  cluster_ca_certificate = base64decode(aws_eks_cluster.main.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = aws_eks_cluster.main.endpoint
+    cluster_ca_certificate = base64decode(aws_eks_cluster.main.certificate_authority[0].data)
+    token                  = data.aws_eks_cluster_auth.cluster.token
   }
 }
 
-provider "aws" {
-  region = "us-east-1"
+data "aws_eks_cluster_auth" "cluster" {
+  name = aws_eks_cluster.main.name
 }
