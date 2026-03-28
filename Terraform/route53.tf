0fd04a48c96bfe2770ca5dabffc1f71d38e1e28e
manojkumardevops89 <manojkumar3177arusam@gmail.com>
2026-03-29 00:46:47 +0530

    Update route53.tf

--- Terraform/route53.tf
+++ Terraform/route53.tf
@@ -3,7 +3,7 @@ data "aws_route53_zone" "main" {
 }
 
 resource "aws_route53_record" "roboshop" {
-  count   = var.alb_dns_name == "" ? 0 : 1   # ✅ KEY FIX
+  count   = var.alb_dns_name == "" ? 0 : 1   
 
   zone_id = data.aws_route53_zone.main.zone_id
   name    = "roboshop.manojdevops897.shop"
