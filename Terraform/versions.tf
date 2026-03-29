f326c9ceede0221c57e814ec948246bd90b817e2
manojkumardevops89 <manojkumar3177arusam@gmail.com>
2026-03-29 09:25:03 +0530

    Update versions.tf

--- Terraform/versions.tf
+++ Terraform/versions.tf
@@ -1,5 +1,12 @@
 terraform {
   required_version = ">= 1.3.0"
+
+  backend "s3" {
+    bucket = "roboshop-terraform-state"
+    key    = "roboshop/terraform.tfstate"
+    region = "us-east-1"
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
