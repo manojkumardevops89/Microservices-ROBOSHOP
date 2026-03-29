48a41a5e001784c592460950d8f23cecb740bf18
manojkumardevops89 <manojkumar3177arusam@gmail.com>
2026-03-29 09:28:18 +0530

    Update outputs.tf

--- Terraform/outputs.tf
+++ Terraform/outputs.tf
@@ -1,11 +1,35 @@
 output "vpc_id" {
-  value = aws_vpc.main.id
+  value       = aws_vpc.main.id
+  description = "VPC ID"
 }
 
 output "eks_cluster_name" {
-  value = aws_eks_cluster.main.name
+  value       = aws_eks_cluster.main.name
+  description = "EKS Cluster Name"
 }
 
 output "eks_cluster_endpoint" {
-  value = aws_eks_cluster.main.endpoint
+  value       = aws_eks_cluster.main.endpoint
+  description = "EKS Cluster Endpoint"
+}
+
+output "nat_gateway_ip" {
+  value       = aws_eip.nat.public_ip
+  description = "NAT Gateway Public IP"
+}
+
+output "public_subnet_ids" {
+  value       = [
+    aws_subnet.public_1a.id,
+    aws_subnet.public_1b.id
+  ]
+  description = "Public Subnet IDs"
+}
+
+output "private_subnet_ids" {
+  value       = [
+    aws_subnet.private_1a.id,
+    aws_subnet.private_1b.id
+  ]
+  description = "Private Subnet IDs"
 }
